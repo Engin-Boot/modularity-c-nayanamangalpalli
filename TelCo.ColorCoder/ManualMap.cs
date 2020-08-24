a779631d15c4c13748af277087e9f082956707fc
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-08-25 01:04:55 +0530

    Update ManualMap.cs

--- TelCo.ColorCoder/ManualMap.cs
+++ TelCo.ColorCoder/ManualMap.cs
@@ -6,8 +6,8 @@ namespace TelCo.ColorCoder
 {
   class ManualMap{
     
-    public static void GetAllMapping(){
-      for(int pairNumber = 1; pairNumber < = 25; pairNumber++){
+    public static void ToString(){
+      for(int pairNumber = 1; pairNumber < 26; pairNumber++){
 
           ColorPair testPair1 = ColorToNumber.GetColorFromPairNumber(pairNumber);
           Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
