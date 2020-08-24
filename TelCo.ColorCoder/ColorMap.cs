1eb5c2eee91ca01c682da16cf1fa6efb41662fae
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-08-25 00:03:01 +0530

    Update and rename ColorPair.cs to ColorMap.cs

--- TelCo.ColorCoder/ColorMap.cs
+++ TelCo.ColorCoder/ColorMap.cs
@@ -5,16 +5,6 @@ using System.Drawing;
 namespace TelCo.ColorCoder
 {
         /// <summary>
-        /// Array of Major colors
-        /// </summary>
-        private static Color[] colorMapMajor;
-        
-        /// <summary>
-        /// Array of minor colors
-        /// </summary>
-        private static Color[] colorMapMinor;
-        
-        /// <summary>
         /// data type defined to hold the two colors of clor pair
         /// </summary>
         internal class ColorPair
@@ -26,12 +16,25 @@ namespace TelCo.ColorCoder
                 return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
             }
         }
+        
+        class ColorMap{
+         /// <summary>
+        /// Array of Major colors
+        /// </summary>
+        private static Color[] colorMapMajor;
+        
         /// <summary>
+        /// Array of minor colors
+        /// </summary>
+        private static Color[] colorMapMinor;
+         /// <summary>
         /// Static constructor required to initialize static variable
         /// </summary>
-        static Program()
+        static ColorMap()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
         }
+       }
+
 }
