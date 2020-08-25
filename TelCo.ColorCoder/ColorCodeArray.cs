851aa4ab9cda687d8758fb95cadd062cbf98eff8
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-08-26 00:07:44 +0530

    Update ColorCodeArray.cs

--- TelCo.ColorCoder/ColorCodeArray.cs
+++ TelCo.ColorCoder/ColorCodeArray.cs
@@ -17,7 +17,7 @@ namespace TelCo.ColorCoder
          /// <summary>
         /// Static constructor required to initialize static variable
         /// </summary>
-        static ColorMap()
+        static ColorCodeArray()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
