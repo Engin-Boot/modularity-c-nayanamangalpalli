b61e7f8a61a6c1a0c855b28cbcd08779ca05ad8b
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-08-25 23:20:14 +0530

    Update and rename ManualMap.cs to ColorCodes.cs

---
+++ TelCo.ColorCoder/ColorCodes.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+  /// <summary>
+  /// data type defined to hold the number and its equivalent color  
+  /// </summary>
+  class ColorCodes{
+    
+    int _PairNumber;
+    ColorPair _ColorPair;
+    
+    /// <summary>
+    /// default constructor required for parametrized constructor  
+    /// </summary>
+    publicColorCodes(){}
+    
+    /// <summary>
+    /// parametrized constructor to initialize class varibles using parameters 
+    /// </summary>
+    public ColorCodes(int number,ColorPair colorPair){
+      _PairNumber = number;
+      _ColorPair = colorPair;
+    }
+    
+    /// <summary>
+    /// Overridden method to return string of number and color 
+    /// </summary>
+    public override void ToString(){
+      return string.Format("Pair Number: {0},Colors: {1}\n", PairNumber, colorPair); 
+    }
+      
+  }
+}
